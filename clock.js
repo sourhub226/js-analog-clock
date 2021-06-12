db01b876e11e922aca24e9dca9d847730db77772
Sourabh Sathe <sou22600@gmail.com>
2021-06-12 21:42:12 +0530

    Tweaked colors

--- clock.js
+++ clock.js
@@ -1,5 +1,5 @@
 function preloadFunc() {
-	var clock = document.querySelector('.clock');
+	var clock = document.querySelector(".clock");
 	if (window.innerWidth > window.innerHeight) {
 		clock.style.width = "75vh";
 		clock.style.height = "75vh";
@@ -9,13 +9,12 @@ function preloadFunc() {
 	}
 	time = new Date();
 	if (time.getHours() >= 6 && time.getHours() < 18) {
-		document.querySelector("body").style.backgroundColor = "#fff";
+		document.querySelector("body").style.backgroundColor = "#F4F4FB";
 	} else {
-		document.querySelector("body").style.backgroundColor = "#222";
+		document.querySelector("body").style.backgroundColor = "#202124";
 	}
 }
 window.onpaint = preloadFunc();
-
 
 setInterval(function () {
 	d = new Date();
@@ -26,7 +25,13 @@ setInterval(function () {
 	mrotation = 6 * mtime;
 	srotation = 6 * stime;
 
-	document.querySelector('.hours').style.transform = `translate(-50%,-100%) rotate(${hrotation}deg)`;
-	document.querySelector('.minutes').style.transform = `translate(-50%,-100%) rotate(${mrotation}deg)`;
-	document.querySelector('.seconds').style.transform = `translate(-50%,-100%) rotate(${srotation}deg)`;
+	document.querySelector(
+		".hours"
+	).style.transform = `translate(-50%,-100%) rotate(${hrotation}deg)`;
+	document.querySelector(
+		".minutes"
+	).style.transform = `translate(-50%,-100%) rotate(${mrotation}deg)`;
+	document.querySelector(
+		".seconds"
+	).style.transform = `translate(-50%,-100%) rotate(${srotation}deg)`;
 }, 1000);
